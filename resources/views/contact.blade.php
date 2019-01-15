49b9c5417b0341ed708d6b56721da6c8dd825894
97033829@st.deltion.nl <stan.klooster@gmail.com>
2019-01-15 14:41:02 +0100

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,20 +11,20 @@
                     <form method="POST" action="">
                         @csrf
 						<div class="form-group">
-							<label for="contactname">Naam *</label>
+							<label for="contactname">Naam <i style="color: red;">*</i></label>
 							<input type="text" class="form-control" id="contactname" required>
 						</div>
 						<div class="form-group">
-							<label for="contactemail">Email *</label>
+							<label for="contactemail">Email <i style="color: red;">*</i></label>
 							<input type="text" class="form-control" id="contactemail" required>
 						</div>
 						<div class="form-group">
-							<label for="contactmessage">Uw bericht *</label>
+							<label for="contactmessage">Uw bericht <i style="color: red;">*</i></label>
 							<textarea class="form-control" id="contactmessage" rows="3" required></textarea>
 						</div>
 						
 						<div class="form-group" width="100%">
-							<label>* Vereist</label>
+							<label><i style="color: red;">*</i> Vereist</label>
 							<button style="margin-left: calc(100% - 139px);" type="submit" class="btn btn-primary">Verstuur</button>
 						</div>	
                     </form>
