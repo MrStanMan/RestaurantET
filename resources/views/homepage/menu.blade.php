047fb871d2c33222c4f07132a61e4c4f47c202c4
MauriceBruijn <bruijn80@gmail.com>
2019-01-16 15:38:42 +0100

    menu page

--- resources/views/homepage/menu.blade.php
+++ resources/views/homepage/menu.blade.php
@@ -1,29 +1,150 @@
 @extends('layouts.menu')
 
 @section('content')
-		<div class="col-lg-3">
-			<div class="panel panel-default" style="height: 80vh; position: inherit; top: 2vh;">
+	<div class="col-lg-3">
+		<div class="panel panel-default" style="height: 80vh; position: inherit; top: 2vh;">
+		
+			<nav class="navbar navbar-inverse" id="sidebar-wrapper" role="navigation">
 			
-				<nav class="navbar navbar-inverse" id="sidebar-wrapper" role="navigation">
-				
-					<ul class="nav sidebar-nav">
-						<li class="sidebar-brand">
-							<b><a>
-								FILTER
-							</b></a>
-						</li>
-						<li>
-							<b><a>3 GANGEN  <span class="glyphicon glyphicon-star" aria-hidden="true"></span></a></b>
-						</li>
-						<li>
-							<b><a>3 GANGEN <span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span></a></b>
-						</li>
-						<li>
-							<b><a>3 GANGEN <span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span></a></b>
-						</li>
-					</ul>
-				</nav>
-				
-			</div>
+				<ul class="nav sidebar-nav">
+					<li class="sidebar-brand">
+						<b><a>
+							FILTER
+						</b></a>
+					</li>
+					<li>
+						<b><a>3 GANGEN  <span class="glyphicon glyphicon-star" aria-hidden="true"></span></a></b>
+					</li>
+					<li>
+						<b><a>3 GANGEN <span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span></a></b>
+					</li>
+					<li>
+						<b><a>3 GANGEN <span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span></a></b>
+					</li>
+				</ul>
+			</nav>
+			
 		</div>
+	</div>
+	
+	<div class="col-lg-9">
+		<div class="panel panel-default" style="height: 80vh; position: inherit; top: 2vh;">
+		
+			<div class="col-lg-4">
+				<div class="db-wrapper">
+					<div class="db-pricing-eleven db-bk-color-one">
+						<div class="product_img">
+							<img src="https://sloanreview.mit.edu/content/uploads/2017/08/MAG-FR-Oestreicher-Singer-Product-Recommendation-Viral-Marketing-Social-Media-Network-Ecommerce-1200-1200x627.jpg"></div> 
+						
+						<div class="db-bk-color-three product_name">
+							product1
+						</div>
+						<div class="db-bk-color-four">
+							<div class="product_description">
+								<p>test</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4">
+				<div class="db-wrapper">
+					<div class="db-pricing-eleven db-bk-color-one">
+						<div class="product_img">
+							<img src="https://sloanreview.mit.edu/content/uploads/2017/08/MAG-FR-Oestreicher-Singer-Product-Recommendation-Viral-Marketing-Social-Media-Network-Ecommerce-1200-1200x627.jpg"></div> 
+						
+						<div class="db-bk-color-three product_name">
+							product1
+						</div>
+						<div class="db-bk-color-four">
+							<div class="product_description">
+								<p>test</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4">
+				<div class="db-wrapper">
+					<div class="db-pricing-eleven db-bk-color-one">
+						<div class="product_img">
+							<img src="https://sloanreview.mit.edu/content/uploads/2017/08/MAG-FR-Oestreicher-Singer-Product-Recommendation-Viral-Marketing-Social-Media-Network-Ecommerce-1200-1200x627.jpg"></div> 
+						
+						<div class="db-bk-color-three product_name">
+							product1
+						</div>
+						<div class="db-bk-color-four">
+							<div class="product_description">
+								<p>test</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4">
+				<div class="db-wrapper">
+					<div class="db-pricing-eleven db-bk-color-one">
+						<div class="product_img">
+							<img src="https://sloanreview.mit.edu/content/uploads/2017/08/MAG-FR-Oestreicher-Singer-Product-Recommendation-Viral-Marketing-Social-Media-Network-Ecommerce-1200-1200x627.jpg"></div> 
+						
+						<div class="db-bk-color-three product_name">
+							product1
+						</div>
+						<div class="db-bk-color-four">
+							<div class="product_description">
+								<p>test</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4">
+				<div class="db-wrapper">
+					<div class="db-pricing-eleven db-bk-color-one">
+						<div class="product_img">
+							<img src="https://sloanreview.mit.edu/content/uploads/2017/08/MAG-FR-Oestreicher-Singer-Product-Recommendation-Viral-Marketing-Social-Media-Network-Ecommerce-1200-1200x627.jpg"></div> 
+						
+						<div class="db-bk-color-three product_name">
+							product1
+						</div>
+						<div class="db-bk-color-four">
+							<div class="product_description">
+								<p>test</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4">
+				<div class="db-wrapper">
+					<div class="db-pricing-eleven db-bk-color-one">
+						<div class="product_img">
+							<img src="https://sloanreview.mit.edu/content/uploads/2017/08/MAG-FR-Oestreicher-Singer-Product-Recommendation-Viral-Marketing-Social-Media-Network-Ecommerce-1200-1200x627.jpg"></div> 
+						
+						<div class="db-bk-color-three product_name">
+							product1
+						</div>
+						<div class="db-bk-color-four">
+							<div class="product_description">
+								<p>test</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			
+					
+		</div>
+	</div>
 @endsection
