5d1337e42b90d9048313f4af2e631fefeb587b76
97033829@st.deltion.nl <stan.klooster@gmail.com>
2019-01-16 10:42:11 +0100

    Minor changes
    2 logos toegevoegd voor review.
    Font size over de complete website wat groter gemaakt. (van 14px -> 16px)
    About page wat uitgebreid.

--- resources/views/homepage/about.blade.php
+++ resources/views/homepage/about.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
 <div class="container">
+	<img src="{{ asset('img/logo_black.png') }}" style="margin-top: 20px;">
 	<h2 class="h2">Over ons</h2>
 	<div class="text">
-		Hier komt wat info te staan over restaurant Excellent Taste.
+		Restaurant excellent taste is een gezellig restaurant met een aantal mooie gerechten.
+		Het restaurant zit in hartje Zwolle, dus dit is een prachtige omgeving om lekker wat te eten!
+	</div>
 </div>
 @endsection
