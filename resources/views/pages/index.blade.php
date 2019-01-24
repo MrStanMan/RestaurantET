29a72577df96183640d563d81cb78de609ccf3f2
MrStanMan <stan.klooster@gmail.com>
2019-01-24 15:31:51 +0100

    Contact + Homepage styling
    Homepage
    
    * Slider
    * Intro text added
    
    Contact
    
    * Added right side for adress
    * Small styling changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,31 @@
 @extends('layouts.base')
 
 @section('content')
-
-		Index
+<div class="homepage">
+	<div id="homepageCarousel" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="http://www.studiomova.nl/wp-content/themes/mova/img/cuttlery.jpg" alt="First slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="http://www.academiabarilla.com/anteprima_pizzamargherita_xhm1p-2_1200.jpg?h=faa3bae42d7180a508c490395249247ded3f362c" alt="Second slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="https://www.24kitchen.nl/files/styles/3x2_960w/public/2017-11/GK120179_ARAB_soep_Tunesische%20soep%20met%20lamsvlees.jpg?itok=KiOUvxne" alt="Third slide">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#homepageCarousel" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#homepageCarousel" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
 	
+	Welkom op de website van Restaurant Excellent Taste! <br>
+	Op deze website kunt u doormiddel van een account reserveren voor een gezellig avondje eten! <br>
+	
+</div>
 @endsection
