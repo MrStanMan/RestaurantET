0f1817bd137c3253f424077c286e4ad3b7d9f972
obejansen <97041816@st.deltion.nl>
2019-01-24 20:15:51 +0100

    fixed merge conflict

--- resources/js/components/ReservationForm.vue
+++ resources/js/components/ReservationForm.vue
@@ -1,15 +1,10 @@
 <template>
-<<<<<<< HEAD
 <div class="row">
 <button class="btn btn-primary" v-on:click.once="checkAdmin">incourante reservatie</button>
 <div v-if="admin ? true : ''">
 	Wanneer je als admin een bestelling doet word de tafel status op gereserveerd staan. De tafels zijn vrij te geven op Deze pagina.
 </div>
-<form action="/reserveer" method="POST" @submit.prevent="onSubmit">
-=======
-
 <form action="/reserveer" method="POST" id="resr" @submit.prevent="onSubmit">
->>>>>>> 1e5b6966ec1eb42c475bb7253069999c3cfef928
 	<div class="alert alert-primary" role="alert" v-if="message != ''" v-model="message">
 		{{ message }}
 	</div>
@@ -22,26 +17,19 @@
             <date-picker filter="moment" v-model="reservation_date" lang="en" type="datetime" value-type="format" format="YYYY-MM-DD HH:mm:ss" :time-picker-options="{ start: '10:00', step: '01:00', end: '22:00' }" confirms :not-before="new Date()"></date-picker>
             <br>
 			<label for="total_guests">Aantal personen</label>
-<<<<<<< HEAD
 			<template v-if="admin == true">
 				<input type="number" class="form-control" name="total_guests" v-model="total_guests"  max="80" min="0">
 			</template>
 			<template v-else>
 				<input type="number" class="form-control" name="total_guests" v-model="total_guests"  max="8" min="0">
 			</template>
-        </div>		
-=======
-			<input type="number" class="form-control" name="total_guests" v-model="total_guests" max="8" min="0">
-			<br>
 			<label for="extra_info">Extra info ( Allergieën, dieet etc. )</label>
 			<textarea form="resr" class="form-control" name="extra_info" v-model="extra_info"></textarea>
-        </div>
-
->>>>>>> 1e5b6966ec1eb42c475bb7253069999c3cfef928
+        </div>		
 		<div class="form-group col-md-4 col-sm-12 col-12 ">
 			<label for="table_nr">Selecteer uw tafel</label>
-			<select class="custom-select" size="10" v-model="selected_table" style="overflow:hidden;">
-				<option v-for="table in tables" v-if="table.status ? 1 : 0">Tafel : {{ table.table_nr }} stoelen : {{ table.total_chairs }}</option>
+			<select class="custom-select" size="10"  style="overflow:hidden;" v-model="selected_table">
+				<option v-for="table in tables" v-if="table.status ? 1 : 0" :value="table.table_nr">Tafel: {{ table.table_nr }} stoelen : {{  }}</option>
 			</select>
 		</div>
 		<div class="col-12 align-items-end">
@@ -92,12 +80,12 @@ export default {
   		onSubmit() {
   			// console.log(this.user);
   			this.reservation = Object.assign({}, this.reservation, {
-  				reservation_nr: this.reservation_date + ' ' + this.selected_time,
-  				time_in: this.selected_time,
-  				date: this.reservation_date,
-  				total_guests: parseInt(this.total_guests),
-  				table_nr: parseInt(this.selected_table),
-  				customer_nr: parseInt(this.customer_nr),
+	  				reservation_nr: this.reservation_date + ' ' + this.selected_time,
+	  				time_in: this.selected_time,
+	  				date: this.reservation_date,
+	  				total_guests: parseInt(this.total_guests),
+	  				table_nr: parseInt(this.selected_table),
+	  				customer_nr: parseInt(this.customer_nr),
 					extra_info: this.extra_info
   			});
   			
